35b5c7c35b6b755018e36990e0f140e6749ca55c
mrblue.eth <mrbluedoteth@gmail.com>
2023-03-05 01:47:28 +0330

    update dux grid -563

--- js/duxgrid.js
+++ js/duxgrid.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 let gridSize = 6;
 let screenSize = 1250;
 let itemSize = 120;
 let walletLoaded = [];
 
-let grid = new Muuri("#duckieGrid", {
+let grid = new Muuri('#duckieGrid', {
   dragEnabled: true,
 });
 
@@ -17,14 +17,14 @@ let initialize = function () {
   screenSize = screen.width;
   itemSize = Math.round((screenSize - 50) / 10);
 
-  $("#duckieGrid").width(gridSize * itemSize);
-  $("#duckieGrid").css("min-height", itemSize + "px");
+  $('#duckieGrid').width(gridSize * itemSize);
+  $('#duckieGrid').css('min-height', itemSize + 'px');
   grid.layout();
 
-  $("#gridsize").on("input", function () {
+  $('#gridsize').on('input', function () {
     gridSize = this.value;
-    $("#rangevalue").text(gridSize);
-    $("#duckieGrid").width(gridSize * itemSize);
+    $('#rangevalue').text(gridSize);
+    $('#duckieGrid').width(gridSize * itemSize);
     grid.layout();
   });
 };
@@ -32,28 +32,28 @@ let initialize = function () {
 initialize();
 
 let addDuckie = function (duckieID) {
-  let item = document.createElement("div");
+  let item = document.createElement('div');
 
-  item.classList.add("item");
-  item.style.width = itemSize + "px";
-  item.style.height = itemSize + "px";
+  item.classList.add('item');
+  item.style.width = itemSize + 'px';
+  item.style.height = itemSize + 'px';
 
-  let itemContent = document.createElement("div");
-  itemContent.classList.add("item-content");
-  itemContent.style.backgroundSize = "contain";
+  let itemContent = document.createElement('div');
+  itemContent.classList.add('item-content');
+  itemContent.style.backgroundSize = 'contain';
 
   $(itemContent).css(
-    "background-image",
+    'background-image',
     "url('data/dux/" + duckieID + ".png')"
   );
 
-  let itemRemove = document.createElement("div");
-  itemRemove.classList.add("item-remove");
+  let itemRemove = document.createElement('div');
+  itemRemove.classList.add('item-remove');
   itemRemove.innerHTML = `<i class="material-icons">&#xE5CD;</i>`;
   itemContent.append(itemRemove);
 
-  let itemID = document.createElement("div");
-  itemID.classList.add("item-id");
+  let itemID = document.createElement('div');
+  itemID.classList.add('item-id');
   itemID.innerHTML = duckieID;
   itemContent.append(itemID);
 
@@ -62,60 +62,60 @@ let addDuckie = function (duckieID) {
   return grid.add([item]);
 };
 
-$(document).on("click", ".item-remove", function () {
+$(document).on('click', '.item-remove', function () {
   let itemToRemove = grid.getItem(this.parentNode.parentNode);
   return grid.remove([itemToRemove], { removeElements: true });
 });
 
-$(".btn-add").click(function () {
-  let duckieID = Number($("#duckieID").val());
-  if (!(duckieID > 0 && duckieID <= 541)) duckieID = 1;
+$('.btn-add').click(function () {
+  let duckieID = Number($('#duckieID').val());
+  if (!(duckieID > 0 && duckieID <= 563)) duckieID = 1;
   addDuckie(duckieID);
 });
 
-$("#duckieID").keypress(function (e) {
+$('#duckieID').keypress(function (e) {
   let key_code = e.which || e.keyCode;
   if (key_code == 13) {
     let duckieID = Number($(this).val());
-    if (!(duckieID > 0 && duckieID <= 541)) duckieID = 1;
+    if (!(duckieID > 0 && duckieID <= 563)) duckieID = 1;
     addDuckie(duckieID);
   }
 });
 
-$("#downloadbutton").click(function () {
-  let canvas = document.createElement("canvas");
-  let context = canvas.getContext("2d");
+$('#downloadbutton').click(function () {
+  let canvas = document.createElement('canvas');
+  let context = canvas.getContext('2d');
   context.imageSmoothingEnabled = false;
 
-  let numOfDuckies = $(".item").length;
+  let numOfDuckies = $('.item').length;
   let numOfRows = Math.ceil(numOfDuckies / gridSize);
 
   canvas.height = 240 * numOfRows;
   canvas.width = 240 * gridSize;
 
-  context.fillStyle = "#908aa0";
+  context.fillStyle = '#908aa0';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  $(".item").each(function (index, element) {
+  $('.item').each(function (index, element) {
     let duckieSVGImage = new Image();
 
     duckieSVGImage.src = $(element)
-      .find(".item-content")
-      .css("background-image")
+      .find('.item-content')
+      .css('background-image')
       .slice(5, -2);
 
-    let itemTransform = $(element).css("transform").split(",");
+    let itemTransform = $(element).css('transform').split(',');
     let itemX = parseInt(itemTransform[4]);
     let itemY = parseInt(itemTransform[5]);
 
     let itemXth = itemX / itemSize;
     let itemYth = itemY / itemSize;
-    if ($(this).hasClass("grayscale")) context.filter = "grayscale(1)";
-    else context.filter = "grayscale(0)";
+    if ($(this).hasClass('grayscale')) context.filter = 'grayscale(1)';
+    else context.filter = 'grayscale(0)';
     context.drawImage(duckieSVGImage, itemXth * 240, itemYth * 240, 240, 240);
   });
-  let a = document.createElement("a");
-  a.href = canvas.toDataURL("image/png");
-  a.download = "DuxGrid.png";
+  let a = document.createElement('a');
+  a.href = canvas.toDataURL('image/png');
+  a.download = 'DuxGrid.png';
   a.click();
 });
--- js/kit.js
+++ js/kit.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 let shuffleArray = function* (array) {
   let i = array.length;
@@ -10,22 +10,22 @@ let shuffleArray = function* (array) {
 
 let changeHeaderDuckies = function () {
   let imageArray = [
-    "burning.gif",
-    "joint.gif",
-    "masks.gif",
-    "nutpfp.gif",
-    "orange-beanie.gif",
-    "rainbow.gif",
-    "raincoat.gif",
-    "robot-eye.gif",
-    "spacesuit.gif",
-    "turban.gif",
-    "zombie.gif",
+    'burning.gif',
+    'joint.gif',
+    'masks.gif',
+    'nutpfp.gif',
+    'orange-beanie.gif',
+    'rainbow.gif',
+    'raincoat.gif',
+    'robot-eye.gif',
+    'spacesuit.gif',
+    'turban.gif',
+    'zombie.gif',
   ];
 
   let randomImage = shuffleArray(imageArray);
-  $(".header-duckie img").each(function (index, element) {
-    element.src = "images/gif-duckies/" + randomImage.next().value;
+  $('.header-duckie img').each(function (index, element) {
+    element.src = 'images/gif-duckies/' + randomImage.next().value;
   });
 };
 
